f137694dd48fce09efbe7a650efbbd8fd24e5d81
dimdroll <dimdroll@gmail.com>
2018-04-07 21:17:30 +0000

    #11: We have changed upsell from easytab to default one. Updated config.xml and Upsell.php to represent it

--- app/code/community/Hackathon/Predictionio/Block/Catalog/Product/List/Upsell.php
+++ app/code/community/Hackathon/Predictionio/Block/Catalog/Product/List/Upsell.php
@@ -21,26 +21,74 @@ class Hackathon_Predictionio_Block_Catalog_Product_List_Upsell extends Mage_Cata
     	$_helper = Mage::helper('predictionio');
         $_model  = Mage::getModel('predictionio/prediction');
     	$product = Mage::registry('product');
+	$productId = $product->getId();
+	
+	// check if we need to disable PredictionIO based on URL params
+	$disablePrediction = Mage::app()->getRequest()->getParam('disableIO');
 
-//    	if ($_helper->isEnabled() && Mage::getSingleton('customer/session')->isLoggedIn()) {
-//	we want recommand even for guests
-// OUTDATED, copy the one from Tab
-	if ($_helper->isEnabled()) {
-	    	if ($similarproducts = $_model->getSimilarProducts($product)) {
-	    	
-	            $collection = Mage::getResourceModel('catalog/product_collection');
-	            Mage::getModel('catalog/layer')->prepareProductCollection($collection);
-	            $collection->addAttributeToFilter('entity_id', array('in' => $similarproducts));
+	$raw_guestData = Mage::getModel('core/cookie')->get('userData');
+	if (isset($raw_guestData->userID)) {
+	        $guestuserID = json_decode($raw_guestData)->userID;
+	}
 
-	            $this->_itemCollection = $collection;
+	// Visitor ID is Magento's internal ID
+	// We need it to link prediction to Visitor
+	// So we can later see what was seen by it
+	$sessionId = Mage::getSingleton("core/session")->getEncryptedSessionId();
+	if ( !isset($sessionId) ) {
+		$sessionId= 'NULL';
+	}
+	// if module is enabled
+	// and if it is not disabled via query string like:
+	// http://107.167.185.249/ginger-sesame-salad-dressing.html?disableIO=false
+	if ($_helper->isEnabled() and !isset($disablePrediction)) {
+		// if user is logged in get his ID
+		if ( Mage::getSingleton('customer/session')->isLoggedIn()) {
+			$customerId = Mage::getSingleton('customer/session')->getCustomerId();
+		// if not, assign ID from guest userData if it exists
+		} elseif (isset($guestuserID)) {
+			$customerId = $guestuserID;
+		}
+		$_helper->logThis('Upsell.php:pre-getRecommendedProducts');
+		// if we have valid customerId recommenend based on it
+		if ( isset($customerId) && $customerId != 'GUEST' ) {
+			$_helper->logThis('User based recommendation');
+			try {
+				$recommendedproducts = $_model->getRecommendedProducts($customerId, 'user', $sessionId, $productId);
+			} catch (Exception $e) {
+				$_helper->logThis('EXCEPTION: ', $e);
+			}
+		}
+		// recommend based on item is user recommendation is empty
+		// or if user recommendation is not applicable
+		if ( !isset($recommendedproducts) ) {
+			$_helper->logThis('Item based recommendation');
+			try {
+		    		$recommendedproducts = $_model->getRecommendedProducts($productId, 'item', $sessionId, $productId);
+			} catch (Exception $e) {
+				$_helper->logThis('EXCEPTION: ', $e);
+			}
+		}
 
-	            return $this->_itemCollection;
+		$_helper->logThis('Upsell.php:pre-recommendedproducts');
+		// if any recommendation returned from engine
+		// show them
+		if (isset($recommendedproducts)) {
 
+			$collection = Mage::getResourceModel('catalog/product_collection');
+			Mage::getModel('catalog/layer')->prepareProductCollection($collection);
+			$collection->addAttributeToFilter('entity_id', array('in' => $recommendedproducts));
+
+			$this->_itemCollection = $collection;
+
+			return $this->_itemCollection;
+
+		// if not, show hand-pick
 	    	} else {
 	    		return parent::_prepareData();
 	    	}	
-    	} else {
-    		return parent::_prepareData();
-    	}       
+	} else {
+		return parent::_prepareData();
+	}
     }
 }
